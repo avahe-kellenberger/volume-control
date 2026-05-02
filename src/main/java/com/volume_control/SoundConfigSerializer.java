7d2a7b3f2f16245a9824695e70123c739ed59ad7
Avahe Kellenberger <avahe@protonmail.ch>
2026-05-01 20:48:08 -0400

    Use client's GSON instance.

--- src/main/java/com/volume_control/SoundConfigSerializer.java
+++ src/main/java/com/volume_control/SoundConfigSerializer.java
@@ -2,13 +2,16 @@ package com.volume_control;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.google.inject.Inject;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SoundConfigSerializer {
-    private static final Gson GSON = new Gson();
+
+    @Inject
+    private static final Gson gson = new Gson();
     private static final Type SOUND_CONFIG_TYPE = new TypeToken<List<SoundConfig>>() {
     }.getType();
 
@@ -20,13 +23,13 @@ public class SoundConfigSerializer {
             if (json == null || json.isEmpty()) {
                 return new ArrayList<>();
             }
-            return GSON.fromJson(json, SOUND_CONFIG_TYPE);
+            return gson.fromJson(json, SOUND_CONFIG_TYPE);
         } catch (Exception e) {
             return new ArrayList<>();
         }
     }
 
     public static String serialize(List<SoundConfig> configs) {
-        return GSON.toJson(configs, SOUND_CONFIG_TYPE);
+        return gson.toJson(configs, SOUND_CONFIG_TYPE);
     }
 }
